368e15e565def3f388c3378688ddd12af0139340
Akari Chan <akariondev@gmail.com>
2023-05-16 05:27:01 +0000

    Nesta versão, algumas melhorias foram feitas:
    1 - O argumento -d (domínio) foi definido como obrigatório usando required=True no argparse.
    
    2 - A formatação de string foi utilizada para construir a consulta de dork para evitar concatenação manual de strings.
    
    3 - Removi a importação do módulo sys que não estava sendo usado.
    
    4 - Removi a verificação desnecessária de args.domain == None e a mensagem de erro correspondente. A verificação é feita usando required=True no argparse.
    
    5 - Removi o comentário sobre o uso do sys.argv[0] para a mensagem de erro, pois isso não é necessário.
    
    6 - O bloco if __name__ == '__main__':
    foi mantido para permitir a execução do script como um módulo importado em outro lugar, se necessário.

---
+++ dorks.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+# GoogD0rk3r 3.0 && Akari
+# Python 3 re-write of version 2.0
+
+import argparse
+from googlesearch import search
+
+
+def main():
+    parser = argparse.ArgumentParser(description='GoogD0rker performs Google dork queries to uncover information about a domain.')
+    parser.add_argument('-d', '--domain', required=True, help='Specify the target domain, e.g., target.com')
+    parser.add_argument('-v', '--verbose', action='store_true', help='Enable verbose mode for debugging purposes')
+    args = parser.parse_args()
+
+    site = args.domain
+    results_list = []
+
+    dorks = {
+        'site': 'site:"{}" inurl:"wp-" OR inurl:"plugin" OR inurl:"upload" OR inurl:"download"'.format(site),
+        'php': 'inurl:"?id=" AND filetype:"php"',
+        'loginPage': 'loginpage.txt'
+    }
+
+    for dork in dorks:
+        if args.verbose:
+            print("Currently Running: {}".format(dorks[dork]))
+
+        search_results = search(dorks[dork], tld='com', lang='en', num=10, start=0, stop=None, pause=2.0)
+
+        results_list.extend(search_results)
+
+    print(results_list)
+
+
+if __name__ == '__main__':
+    main()
+
